bbb0a2c4af9a39f31f8168e9b2ac6bed157ee84b
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-08-08 18:10:26 +0530

    #005 | Sourav | Basic configuration for blog post page.

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -15,19 +15,12 @@ export class BlogService {
 
     public async getBlogById(id: string): Promise<Blog> {
         const baseUrl = this.configHandler.getBlogServiceUrl();
-        return this.http.get<Blog>(`${baseUrl}/blogs?${id}`).toPromise();
+        return await this.http.get<Blog>(`${baseUrl}/blogs/${id}`).toPromise();
     }
 
     public async getBlogList(pageable: Pageable): Promise<Blog[]> {
         const baseUrl = this.configHandler.getBlogServiceUrl();
-        console.log(baseUrl);
-        `accept, content-type`
-        const httpHeaders = new HttpHeaders();
-        httpHeaders.set('Access-Control-Allow-Headers', ['accept', 'content-type']);
-        httpHeaders.set('Access-Control-Allow-Methods', 'POST');
-        httpHeaders.set('Access-Control-Allow-Origin', '*');
-        const options = {headers: httpHeaders};
-        const responseBody = await this.http.get(`${baseUrl}/blogs?${pageable.toString()}`, options).toPromise();
+        const responseBody = await this.http.get(`${baseUrl}/blogs?${pageable.toString()}`).toPromise();
         return responseBody['content'];
     }
 }
--- src/app/views/pages/post/post.component.ts
+++ src/app/views/pages/post/post.component.ts
@@ -1,17 +1,25 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Blog} from "../../../models/blog";
+import {BlogService} from "../../../services/blog.service";
+import {ConfigHandler} from "../../../configs/config-handler";
 
 @Component({
     selector: 'app-post',
     templateUrl: 'post.component.html',
-    styleUrls: ['./post.component.css']
+    styleUrls: ['./post.component.css'],
+    providers: [BlogService, ConfigHandler]
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
     @Input()
-    public blog: Blog;
+    public blogId: string;
 
-    constructor() {
+    public blog : Blog
+
+    constructor(private blogService: BlogService) {
     }
 
+    async ngOnInit(): Promise<void> {
+        this.blog = await this.blogService.getBlogById('ff808181731d5d7d01731d5e5b5a0000');
+    }
 }
